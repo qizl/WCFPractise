7f728199d1bfb6618edc35c57cf251d5c85131cf
qizl <q@qizl.cn>
2018-06-17 11:47:36 +0800

    -

--- WcfClient/Controllers/HomeController.cs
+++ WcfClient/Controllers/HomeController.cs
@@ -11,19 +11,5 @@ namespace WcfClient.Controllers
 
             return View(students);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
