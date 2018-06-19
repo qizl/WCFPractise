8c0975661d1cd35f124f3d137b7173831ce65e7a
qizl <q@qizl.cn>
2018-06-19 11:50:34 +0800

    -

---
+++ WcfServices/Clients/Program.cs
@@ -0,0 +1,19 @@
+﻿using Clients.CalculatorService;
+using System;
+
+namespace Clients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CalculatorServiceClient proxy = new CalculatorServiceClient())
+            {
+                Console.WriteLine($"x + y = {proxy.Add(1, 2)} when x = {1} and y = {2}");
+                Console.WriteLine($"x - y = {proxy.Subtract(1, 2)} when x = {1} and y = {2}");
+                Console.WriteLine($"x * y = {proxy.Multiply(1, 2)} when x = {1} and y = {2}");
+                Console.WriteLine($"x / y = {proxy.Divide(1, 2)} when x = {1/2} and y = {2}");
+            }
+        }
+    }
+}
--- WcfServices/Services/CalculatorService.cs
+++ WcfServices/Services/CalculatorService.cs
@@ -6,10 +6,10 @@ namespace Services
     {
         public double Add(double x, double y) => x + y;
 
-        public double Divide(double x, double y) => x - y;
+        public double Subtract(double x, double y) => x - y;
 
         public double Multiply(double x, double y) => x * y;
 
-        public double Subtract(double x, double y) => x / y;
+        public double Divide(double x, double y) => x / y;
     }
 }
